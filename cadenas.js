36b2b4234c04c817276f8e66988b30e504c037df
rafael22 <rafaelcarmona295@hmail.com>
2023-01-08 12:17:36 -0400

    Subiendo otro

---
+++ cadenas.js
@@ -0,0 +1,28 @@
+let nom = "Rafael";
+let ape = "Carmona";
+
+let estudiante = "Rafael" +" "+"Carmona";
+console.log(estudiante);
+
+let estudianteMinus = estudiante;
+console.log(estudianteMinus.toLowerCase());
+
+let estudianteMayus = estudiante;
+console.log(estudiante.toUpperCase());
+
+let numcar = estudiante;
+
+console.log(numcar.length);
+
+let primera = nom;
+console.log(nom.charAt());
+
+let ultima = ape;
+console.log(ultima.slice(-1));
+
+let cf = estudiante;
+console.log(cf.includes("Rafael"));
+
+let eliminar = estudiante;
+console.log(eliminar.trim());
+
